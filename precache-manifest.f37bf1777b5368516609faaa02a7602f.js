67bd8ac63c1acba46bdd024e58e71e5b4e9f84f7
jhvoong <jhvoong@ucdavis.edu>
2020-07-08 22:17:21 -0700

    Updates

---
+++ precache-manifest.f37bf1777b5368516609faaa02a7602f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc970a5a76ab28c5852a2e3d7e9270a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20ce666bcbe46cf0fdcc",
+    "url": "/static/css/main.4835f77f.chunk.css"
+  },
+  {
+    "revision": "37a4c1d2e14343d0e1c7",
+    "url": "/static/js/2.889a6e18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.889a6e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ce666bcbe46cf0fdcc",
+    "url": "/static/js/main.aa9dbd3f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/static/media/Proxima_Nova_Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/Proxima_Nova_Regular.410504d4.otf"
+  },
+  {
+    "revision": "46381f7aaa9767c79e11470cc8744f1e",
+    "url": "/static/media/bitproject.46381f7a.jpg"
+  },
+  {
+    "revision": "af6fb97162774949567bd4d4a31d49dc",
+    "url": "/static/media/healthyu.af6fb971.jpg"
+  },
+  {
+    "revision": "3ae48039a5c1e4cc51232ec16139b86a",
+    "url": "/static/media/songswithfriends.3ae48039.jpg"
+  }
+]);
